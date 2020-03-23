67eae654ae924cafd02dd55d96b89fc8d4ae986e
Guillermo Lara <grlara9@gmail.com>
2020-03-23 15:44:21 -0700

    work on routes and created pages folder

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 
 const app = new express()
 
@@ -7,6 +8,18 @@ const PORT = 4000;
 //Express will expect all static assets to be in public
 app.use(express.static('public'))
 
+app.get('/', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+})
+app.get('/about', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+app.get('/about', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+})
+app.get('/about', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
 app.listen(PORT, ()=> {
     console.log("Listening on PORT: " + PORT)
 })
