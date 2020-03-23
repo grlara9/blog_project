a56e01477ae16a0c5fcee1d9c6eb2b7acbdbe241
Guillermo Lara <grlara9@gmail.com>
2020-03-23 15:08:16 -0700

    downloaded blog theme from bootstrap

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = new express()
 
 const PORT = 4000;
 
+//Express will expect all static assets to be in public
+app.use(express.static('public'))
+
 app.listen(PORT, ()=> {
     console.log("Listening on PORT: " + PORT)
 })
