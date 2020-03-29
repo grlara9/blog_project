94e4b26a89b3facf5866dcb8847fc65b83e0b51c
Guillermo Lara <grlara9@gmail.com>
2020-03-28 17:28:35 -0700

    created models folder and establish connection with database

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require('express')
 var exphbs = require("express-handlebars")
 const path = require('path')
-
+const bodyParser= require('body-parser')
+const mongoose = require('mongoose')
 const app = new express()
 
 const PORT = 4000;
+
+const BlogPost = require('./models/BlogPost')
+
+mongoose.connect("mongodb://localhost/my_database", { useNewUrlParser: true })
 
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true} ))
+
 //Express will expect all static assets to be in public
 app.use(express.static('public'))
 
 app.get('/', (req, res) =>{
-   res.render('index')
+    const blogposts = BlogPost.find({})
+    console.log("this is the result:" + blogposts)
+   res.render('index', {blogposts: blogposts})
 })
 app.get('/about', (req,res)=>{
     res.render('about')
@@ -28,7 +38,12 @@ app.get('/contact', (req,res)=>{
 app.get('/posts/new', (req, res)=>{
     res.render('create')
 })
-
+app.post('/posts/store', (req, res)=>{
+    console.log(req.body)
+    BlogPost.create(req.body, (error, blogpost) => {
+    res.redirect('/')
+    })
+})
 app.listen(PORT, ()=> {
     console.log("Listening on PORT: " + PORT)
 })
