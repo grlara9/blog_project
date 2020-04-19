d1e4274831f60efd2cba2a1c0886b41bcfc39797
Guillermo Lara <grlara9@gmail.com>
2020-04-19 15:42:47 -0700

    create delete method and use axios to connect to backend

--- blog-app/src/components/blog-list.component.js
+++ blog-app/src/components/blog-list.component.js
@@ -1,12 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
 class BlogList extends React.Component {
-    render() {
+  constructor(props){
+    super(props);
+      //.deleteBlog = this.deleteBlog.bind(this);
+
+      this.state ={
+        blogs: []
+      }
+  }  
+  componentDidMount() {
+    axios.get('http://localhost:5000/blog/')
+    .then(response => {
+      console.log(response)
+      this.setState({
+        blogs: response.data
+      })
+    })
+    .catch((error) => {
+    console.log(error)
+    })
+
+  }
+    
+  
+  render() {
         return (
-            <div>
-                this is the blog list
-            </div>
-        )
+          <div>
+      <Button color="danger"></Button>
+      <Modal>
+        <ModalHeader>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary">Do Something</Button>{' '}
+          <Button color="secondary">Cancel</Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+         
+         )
     }
 }
 export default BlogList
--- blog-app/src/components/navbar.component.js
+++ blog-app/src/components/navbar.component.js
@@ -2,27 +2,26 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class Navbar extends React.Component {
-    render() {
-        return (
-           <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-              <Link to="/" className="navbar-brand" href="index.html">Soccer Blog</Link>
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                  Menu
-                  <i className="fas fa-bars"></i>
-                </button>
-                <div className="collapse navbar-collapse">
-                  <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                       <Link to="/" className="nav-link">All Blogs</Link>
-                    </li>
-                    <li className="nav-item">
-                      <Link to="/blog" className="nav-link">New Blog</Link>
-                    </li>
-                  </ul>
-                </div>
-             
-            </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link to="/" className="navbar-brand" href="index.html">Soccer Blog</Link>
+          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i className="fas fa-bars"></i>
+          </button>
+          <div className="collapse navbar-collapse">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+                <Link to="/" className="nav-link">All Blogs</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/blog" className="nav-link">Add Blog</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    )
+  }
 }
 export default Navbar;
