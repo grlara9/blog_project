67ea3ba40137716e273414408da8f6261930fc13
Guillermo Lara <grlara9@gmail.com>
2020-04-20 14:24:51 -0700

    create new component create.user

---
+++ blog-app/src/components/create-user.component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class CreateUser extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default CreateUser
