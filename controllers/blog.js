86f1f3677460f655efe00901e370fde5964772b6
Guillermo Lara <grlara9@gmail.com>
2020-04-30 22:11:14 -0700

    created route to get a specific blog and update blog

--- controllers/blog.js
+++ controllers/blog.js
@@ -27,10 +27,31 @@ router.post("/add", (req, res)=>{
         .catch(err=> res.status(400).json("Error: " + err))
     });
 
+    router.get("/:id", (req, res)=>{
+        BlogPost.findById(req.params.id)
+        .then(response => res.json(response))
+        .catch(err=>res.status(400).json("Error:" + err))
+    })
+
     router.delete("/:id", (req, res)=>{
         BlogPost.findByIdAndDelete(req.params.id)
         .then(()=>res.json("Blog deleted!"))
         .catch(err=>res.status(400).json("Error: " + err));
+    })
+
+    router.put("/update/:id", (req, res)=>{
+        BlogPost.findById(req.params.id)
+        .then(response =>{
+            response.title= req.body.title;
+            response.blog = req.body.blog;
+            response.username = req.body.username;
+            response.date = Date.parse(req.body.date)
+
+            response.save()
+            .then(() => res.json("Blog Updated"))
+            .catch(err => res.status(400).json("Error: " + err));
+        })
+        .catch(err => res.status(400).json("Error: " + err))
     })
 
 
