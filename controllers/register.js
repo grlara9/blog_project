1fc56562483acfb56fa3d917fa870a764d58907c
Guillermo Lara <grlara9@gmail.com>
2020-05-08 13:18:36 -0700

    finish registration route

--- controllers/register.js
+++ controllers/register.js
@@ -4,44 +4,42 @@ const User = require('../models/UserRegister')
 const bcrypt = require('bcrypt')
 
 router.post("/register",(req, res) => {
-    try {
-      let { email, password, passwordCheck, username } = req.body;
-  
-      // validate
-  
-      if (!email || !password || !passwordCheck)
-        return res.status(400).json({ msg: "Not all fields have been entered." });
-      if (password.length < 5)
-        return res
-          .status(400)
-          .json({ msg: "The password needs to be at least 5 characters long." });
-      if (password !== passwordCheck)
-        return res
-          .status(400)
-          .json({ msg: "Enter the same password twice for verification." });
-  
-      const existingUser =User.findOne({ email: email });
-      console.log(existingUser)
-      if (existingUser)
-        return res
-          .status(400)
-          .json({ msg: "An account with this email already exists." });
-  
-      
-  
-      const salt = bcrypt.genSalt();
-      const passwordHash =  bcrypt.hash(password, salt);
-  
-      const newUser = new User({
-        email,
-        password: passwordHash,
-        displayName,
-      });
-      const savedUser =  newUser.save();
-      res.json(savedUser);
-    } catch (err) {
-      res.status(500).json({ err });
-    }
-  });
+   let { email, password, confirmPass } = req.body;
 
+   if (!email || !password || !confirmPass)
+    return res.status(400).json({ msg: "Not all fields have been entered." });
+    if (password.length < 5)
+    return res
+     .status(400)
+     .json({ msg: "The password needs to be at least 5 characters long." });
+ if (password !== confirmPass)
+   return res
+     .status(400)
+     .json({ msg: "Enter the same password twice for verification." });
+
+
+      User.findOne({email: email})
+        .then(user => {
+          
+          if (!user) {
+            bcrypt.hash(password, 10, (err, hash) => {
+            password = hash
+
+            const newUser = new User({
+              email,
+              password
+              })
+
+              newUser.save()
+              .then(()=> res.json("Registered! "))
+              .catch(err => res.status(400).json("Error: " + err))
+             })
+
+          } else {
+            res.json({ error: 'User already exists' })
+          }
+        })
+
+        .catch(err => {res.send('error: ' + err)})
+      })
 module.exports = router
