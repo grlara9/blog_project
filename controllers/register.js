f2df32183309d54bc6e53fc697251fe693e09d5a
Guillermo Lara <grlara9@gmail.com>
2020-05-08 19:59:59 -0700

    finish with login route added some validation and once login create token

--- controllers/register.js
+++ controllers/register.js
@@ -2,35 +2,29 @@ var express = require("express")
 var router = express.Router()
 const User = require('../models/UserRegister')
 const bcrypt = require('bcrypt')
+const jwt = require("jsonwebtoken");
 
 router.post("/register",(req, res) => {
-   let { email, password, confirmPass } = req.body;
+  let { email, password, confirmPass } = req.body;
 
-   if (!email || !password || !confirmPass)
+  if (!email || !password || !confirmPass)
     return res.status(400).json({ msg: "Not all fields have been entered." });
-    if (password.length < 5)
-    return res
-     .status(400)
-     .json({ msg: "The password needs to be at least 5 characters long." });
- if (password !== confirmPass)
-   return res
-     .status(400)
-     .json({ msg: "Enter the same password twice for verification." });
+  if (password.length < 5)
+    return res.status(400).json({ msg: "The password needs to be at least 5 characters long." });
+  if (password !== confirmPass)
+    return res.status(400).json({ msg: "Enter the same password twice for verification." });
 
+    User.findOne({email: email})
+      .then(user => {
+        if (!user) {
+          bcrypt.hash(password, 10, (err, hash) => {
+          password = hash
 
-      User.findOne({email: email})
-        .then(user => {
-          
-          if (!user) {
-            bcrypt.hash(password, 10, (err, hash) => {
-            password = hash
-
-            const newUser = new User({
-              email,
-              password
-              })
-
-              newUser.save()
+          const newUser = new User({
+            email,
+            password
+          })
+          newUser.save()
               .then(()=> res.json("Registered! "))
               .catch(err => res.status(400).json("Error: " + err))
              })
@@ -41,5 +35,35 @@ router.post("/register",(req, res) => {
         })
 
         .catch(err => {res.send('error: ' + err)})
+      })
+
+router.post("/login", (req, res) =>{
+  const { email, password} = req.body
+    User.findOne({
+      email: email
+    }).then(user =>{
+      console.log(user)
+      if(user){
+        if(bcrypt.compare(password, user.password)){
+
+          const data = {
+            _id: user._id,
+            email: user.email,
+
+          }
+          let token = jwt.sign(data, process.env.JWT_SECRET,{
+            expiresIn: 1440
+          })
+          res.json(token)
+        }else{
+          res.json({error: "User combination incorrect"})
+        }
+      }else{
+        res.json({error: "User combination incorrect"})
+      }
+    })
+    .catch(err => {
+      res.json("Error: " + err)
+    })
       })
 module.exports = router
