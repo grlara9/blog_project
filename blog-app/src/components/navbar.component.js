af89491a41975b12d8f371498f9b1d18c9fbe3a3
Guillermo Lara <grlara9@gmail.com>
2020-05-18 22:48:16 -0700

    nav bar edit link so if user is logged in logout link apear, if logget out the register and login links shows

--- blog-app/src/components/navbar.component.js
+++ blog-app/src/components/navbar.component.js
@@ -2,29 +2,75 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class Navbar extends React.Component {
+
+  logOut = (e) => {
+    e.preventDefault()
+    localStorage.removeItem('usertoken')
+    this.props.history.push(`/`)
+  }
+
   render() {
+    const loginRegLink = (
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            Register
+          </Link>
+        </li>
+      </ul>
+    )
+
+    const userLink = (
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link to="/profile" className="nav-link">
+            User
+          </Link>
+        </li>
+        <li className="nav-item">
+          <a href="" onClick={this.logOut} className="nav-link">
+            Logout
+          </a>
+        </li>
+      </ul>
+    )
+  
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link to="/" className="navbar-brand" href="index.html">Soccer Blog</Link>
-          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i className="fas fa-bars"></i>
-          </button>
-          <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ml-auto">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark rounded">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarsExample10"
+          aria-controls="navbarsExample10"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div
+          className="collapse navbar-collapse justify-content-md-center"
+          id="navbarsExample10"
+        >
+          <ul className="navbar-nav">
             <li className="nav-item">
-                <Link to="/" className="nav-link">All Blogs</Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/blog" className="nav-link">Add Blog</Link>
-            </li>
-            <li>
-              <Link to="/user" className="nav-link">Add User</Link>
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
             </li>
           </ul>
+          {localStorage.usertoken ? userLink : loginRegLink}
         </div>
       </nav>
     )
   }
 }
+export default withRouter(Navbar)
+
 export default Navbar;
